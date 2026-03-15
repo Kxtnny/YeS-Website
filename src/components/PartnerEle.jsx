343ebc2c42c1ba38e2bbe1efb3905d7601686768
Acrylic125 <acrylic125email@gmail.com>
2026-03-15 12:46:55 +0800

    chore: init

--- src/components/PartnerEle.jsx
+++
@@ -1,9 +0,0 @@
-export default function PartnerEle(props){
-    return(
-        <div className="partner-container">
-            <div className="partner-img-container">
-                <img src={props.img} alt={props.name} className="partner-logo"/>
-            </div>
-        </div>
-    )
-}
